ccb1641a43db344409dd1940b26aa6d08984c7b8
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-07-29 23:16:15 -0300

    feat: implements features and general adjustments in the project

--- server/src/http/routes/create-question.ts
+++ server/src/http/routes/create-question.ts
@@ -2,8 +2,10 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
 import { schema } from "../../db/schema/index.ts";
 import { db } from "../../db/connection.ts";
+import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts";
+import { and, eq, sql } from "drizzle-orm";
 
-export const createQuestionRoute: FastifyPluginCallbackZod = async (app) => {
+export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
     "/rooms/:roomId/questions",
     {
@@ -16,15 +18,47 @@ export const createQuestionRoute: FastifyPluginCallbackZod = async (app) => {
         }),
       },
     },
+
     async (req, reply) => {
       const { roomId } = req.params;
       const { question } = req.body;
+
+      const embeddings = await generateEmbeddings(question);
+
+      const embeddingsAsString = `[${embeddings.join(",")}]`;
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3);
+
+      let answer: string | null = null;
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription);
+
+        answer = await generateAnswer(question, transcriptions);
+      }
 
       const result = await db
         .insert(schema.questions)
         .values({
           roomId,
           question,
+          answer,
         })
         .returning();
 
@@ -34,7 +68,10 @@ export const createQuestionRoute: FastifyPluginCallbackZod = async (app) => {
         throw new Error("Failed to create new question.");
       }
 
-      return reply.status(201).send({ questionId: insertedQuestion.id });
+      return reply.status(201).send({
+        questionId: insertedQuestion.id,
+        answer,
+      });
     }
   );
 };
---
+++ web/src/pages/record-room-audio.tsx
@@ -0,0 +1,113 @@
+/* biome-ignore-all lint/suspicious/noConsole: <explanation> */
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === "function" &&
+  typeof window.MediaRecorder === "function";
+
+type RoomParams = {
+  roomId: string;
+};
+
+export function RecordRoomAudio() {
+  const params = useParams<RoomParams>();
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout>(null);
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (recorder?.current && recorder.current.state !== "inactive") {
+      recorder.current.stop();
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+
+    formData.append("file", audio, "audio.webm");
+
+    const res = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const result = await res.json();
+
+    console.log(result);
+  }
+
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: "audio/webm",
+      audioBitsPerSecond: 64_000,
+    });
+
+    recorder.current.ondataavailable = (e) => {
+      if (e.data.size > 0) {
+        uploadAudio(e.data);
+      }
+    };
+
+    recorder.current.onstart = () => {
+      console.log("Gravação iniciada!");
+    };
+
+    recorder.current.onstop = () => {
+      console.log("Gravação encerrada/pausada");
+    };
+
+    recorder.current.start();
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert("O seu navegador não suporta gravação");
+      return;
+    }
+
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    createRecorder(audio);
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+
+      createRecorder(audio);
+    }, 5000);
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center gap-3 flex-col">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Pausar gravação</Button>
+      ) : (
+        <Button onClick={startRecording}>Gravar áudio</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  );
+}
